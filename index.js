314396c9dcad7d76cf38db200d46688f5839f33f
vimal2033 <vimalmishra2033@gmail.com>
2025-07-05 17:53:12 +0530

    initial files

---
+++ index.js
@@ -0,0 +1,13 @@
+import jsonfile from 'jsonfile';
+import moment from 'moment';
+import simpleGit from 'simple-git';
+
+const path = 'data.json';
+const date=moment().format();
+const data = {
+  date: date
+};
+
+jsonfile.writeFile(path, data,);
+
+simpleGit().add([path]).commit(Date,{'--DATE': date}).push('origin', 'main');
